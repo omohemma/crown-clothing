7c5c4bf33579038bd0227befdc52d821d2841fbb
omohemma <emmaomoh@gmail.com>
2020-01-20 03:19:34 +0100

    Setting Up Folder Structure

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -11,36 +11,35 @@ const Homepage = () => {
             <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
-      
 
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">JACKETS</h1>
-          <span className="subtitle">SHOP NOW</span>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">JACKETS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SNICKERS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">WOMENS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">MENS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
         </div>
       </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">SNICKERS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">WOMENS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MENS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-    </div>
     </div>
   );
 };
