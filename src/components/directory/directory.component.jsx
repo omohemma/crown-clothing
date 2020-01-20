500e2bc33e38597c0cf522564a72e4302de6569b
omohemma <emmaomoh@gmail.com>
2020-01-20 04:04:47 +0100

    Dynamic Directory

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,16 +2,57 @@ import React from "react";
 import MenuItem from "../menu-item/menu-item.component";
 import "./directory.styles.scss";
 
-const DirectoryItem = () => {
-  return (
-    <div className="directory-menu">
-      <MenuItem title={"HATS"} subtitle={"SHOP NOW"} />
-      <MenuItem title={"JACKETS"} subtitle={"SHOP NOW"} />
-      <MenuItem title={"SNICKERS"} subtitle={"SHOP NOW"} />
-      <MenuItem title={"WOMENS"} subtitle={"SHOP NOW"} />
-      <MenuItem title={"MENS"} subtitle={"SHOP NOW"} />
-    </div>
-  );
-};
+class Directory extends React.Component {
+  constructor() {
+    super();
 
-export default DirectoryItem;
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          id: 1,
+          linkUrl: "shop/hats"
+        },
+        {
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          id: 2,
+          linkUrl: "shop/jackets"
+        },
+        {
+          title: "sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          id: 3,
+          linkUrl: "shop/sneakers"
+        },
+        {
+          title: "womens",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          size: "large",
+          id: 4,
+          linkUrl: "shop/womens"
+        },
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          size: "large",
+          id: 5,
+          linkUrl: "shop/mens"
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(section => (
+          <MenuItem title={section.title} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, subtitle }) => {
+const MenuItem = ({ title }) => {
   return (
     <div className="menu-item">
       <div className="content">
         <h1 className="title">{title}</h1>
-        <span className="subtitle">{subtitle}</span>
+        <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
   );
