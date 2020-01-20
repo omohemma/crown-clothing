2f7c575c59ae604a27a69f0a2e37b0cd1c563daa
omohemma <emmaomoh@gmail.com>
2020-01-20 03:33:29 +0100

    Menu Item Component

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./menu-item.styles.scss";
+
+const MenuItem = ({ title, subtitle }) => {
+  return (
+    <div className="menu-item">
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">{subtitle}</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,44 +1,16 @@
 import React from "react";
+import MenuItem from "../../components/menu-item/menu-item.component";
 import "./homepage.styles.scss";
 
 const Homepage = () => {
   return (
     <div className="homepage">
       <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">JACKETS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">SNICKERS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">WOMENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">MENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
+        <MenuItem title={"HATS"} subtitle={"SHOP NOW"} />
+        <MenuItem title={"JACKETS"} subtitle={"SHOP NOW"} />
+        <MenuItem title={"SNICKERS"} subtitle={"SHOP NOW"} />
+        <MenuItem title={"WOMENS"} subtitle={"SHOP NOW"} />
+        <MenuItem title={"MENS"} subtitle={"SHOP NOW"} />
       </div>
     </div>
   );
