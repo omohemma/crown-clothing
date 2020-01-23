aa269eb3e0569e6c0045a226152ff7e59e822147
omohemma <emmaomoh@gmail.com>
2020-01-23 22:17:12 +0100

    Integrate Routing to Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import  Homepage from "./pages/homepage/homepage.component";
+import { Route } from "react-router-dom";
+import Homepage from "./pages/homepage/homepage.component";
 import "./App.css";
+
+//Functional Component to implement Routing
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
 
 // Import All Pages into The Application Container
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact path="/" component={Homepage} />
+      <Route path="/hats" component={HatsPage} />
     </div>
   );
 }
